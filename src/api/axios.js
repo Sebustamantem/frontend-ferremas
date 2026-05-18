ea12cc7bddf0120ba09973d9191a8fe09c54cd23
Sebastián Patricio <se.bustamantem@duocuc.cl>
2026-05-17 21:51:31 -0400

    Problema de conexion

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,23 +1,17 @@
-import axios from "axios";
+import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000"
 
 const api = axios.create({
     baseURL: `${API_URL}/api`,
-});
+})
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-
+    const token = localStorage.getItem("token")
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`
     }
+    return config
+})
 
-    return config;
-});
-
-
-export const createPaymentPreference = (address) =>
-    api.post("/payment/create-preference", { address })
-
-export default api;
+export default api
